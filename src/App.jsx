b193301e4b3bf43ee84e01029fb9cbadd67cb781
amin-bani-a <amin.baniamerian67@gmail.com>
2023-08-01 13:37:26 +0400

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,30 @@
+import { useState } from "react";
+import Title from "./Title";
+import menu from "./data";
+import Menu from "./Menu";
+import Categories from "./Categories";
+
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
+
 const App = () => {
-  return <h2>Menu Starter</h2>;
+  const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(menu);
+    } else {
+      const newItems = menu.filter((item) => item.category === category);
+      setMenuItems(newItems);
+    }
+  };
+  return (
+    <main>
+      <section className="menu">
+        <Categories categories={categories} filterItems={filterItems} />
+        <Title text={"our menu"} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 };
 export default App;
